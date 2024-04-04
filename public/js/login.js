b3012a9fcfd0e09629a58f61b93a763f56f8d80e
Nihsadd <nihsadd@gmail.com>
2024-04-03 20:55:22 -0600

    add login/logout scripts

---
+++ public/js/login.js
@@ -0,0 +1,53 @@
+//Login
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim(); //change '#email-login' to the correct id for email input field
+    const password = document.querySelector('#password-login').value.trim(); //change '#password-login' to the correct id for password input field
+
+    if (email && password) {
+        const response = await fetch('api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+    if (response.ok) {
+        document.location.replace('/');
+        } else {
+            alert('Failed to log in.');
+        }
+    }
+};
+
+//Signup
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+    if (response.ok) {
+        document.location.replace('/');
+        } else {
+            alert('Failed to sign up.');
+        }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
