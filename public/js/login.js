9323fb656679d117e61c393a989dd36167b18989
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-04-06 17:58:20 -0600

    Style to homepage

--- public/js/login.js
+++ public/js/login.js
@@ -13,40 +13,26 @@ const loginFormHandler = async (event) => {
         });
 
     if (response.ok) {
-        document.location.replace('/api/coffeeshops/:id');
+        // document.location.replace('/');
+        alert('Logged in successfully!');
         } else {
             alert('Failed to log in.');
         }
     }
 };
 
-//Signup
-
-// const signupFormHandler = async (event) => {
-//     event.preventDefault();
-
-//     const username = document.querySelector('#username-signup').value.trim();
-//     const email = document.querySelector('#email-signup').value.trim();
-//     const password = document.querySelector('#password-signup').value.trim();
-
-//     if (username && email && password) {
-//         const response = await fetch('api/users', {
-//             method: 'POST',
-//             body: JSON.stringify({ username, email, password }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
-
-//     if (response.ok) {
-//         document.location.replace('/');
-//         } else {
-//             alert('Failed to sign up.');
-//         }
-//     }
-// };
 
 document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
+
+const labels = document.querySelectorAll('.form-group-login label');
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+});
 
 // document
 //     .querySelector('.signup-form')
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,44 +1,44 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
-    if (username && email && password) {
-        const response = await fetch('/api/users/', {
+  if (username && email && password) {
+    const response = await fetch('/api/users/', {
+      method: 'POST',
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      const emailResponse = await fetch('/send-signup-email', {
         method: 'POST',
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ email }),
         headers: { 'Content-Type': 'application/json' },
-        });
+      });
+      if (emailResponse.ok) {
+        console.log('Email sent successfully');
+      } else {
+        console.error('Error sending email');
+      }
 
-        if (response.ok) {
-            const emailResponse = await fetch('/send-signup-email', {
-                method: 'POST',
-                body: JSON.stringify({ email }),
-                headers: { 'Content-Type': 'application/json' },
-                });
-                if (emailResponse.ok) {
-                    console.log('Email sent successfully');
-                  } else {
-                    console.error('Error sending email');
-                  }
-            
-                  document.location.replace('/');
-                } else {
-                  alert(response.statusText);
-                }
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
+  }
 };
-  
+
 
 document
-.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  .querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 
-// const labels = document.querySelectorAll('.form-group-login label');
-//     labels.forEach(label => {
-//         label.innerHTML = label.innerText
-//             .split('')
-//             .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
-//             .join('')
-//     });
+const tags = document.querySelectorAll('.form-group-login label');
+tags.forEach(label => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('')
+});
