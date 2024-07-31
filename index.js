d8ef6928fed3d2f6d5a42cfce966edc14c23adcf
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-07-31 11:15:40 -0600

    .

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 const hbs = exphbs.create({ helpers });
 
